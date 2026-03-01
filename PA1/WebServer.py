e0cd0f3556de3517956a8a947906a8407fd16629
Dylan Ravel <dylanm.ravel@gmail.com>
2026-02-28 18:40:16 -0800

    Fix

--- PA1/WebServer.py
+++ PA1/WebServer.py
@@ -1,32 +1,43 @@
+# Full Name: Dylan Ravel
+# Student ID: 2445987
+# Chapman Email: ravel@chapman.edu
+# Course Number and Section: CPSC-353-03
+# Assignment: Prog-Assign -1 : Web Server Note
+
+
 from socket import *
 import sys
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
+HOST = ""
+PORT = 6789
+BUFFER_SIZE = 1024
 
-serverSocket.bind(("", 6789))
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((HOST, PORT))
 serverSocket.listen(1)
 
 while True:
-    print('Ready to serve...')
+    print("Ready to serve...")
     connectionSocket, addr = serverSocket.accept()
-    
+    print(f"Connection from {addr}")
+
     try:
-        message = connectionSocket.recv(1024).decode()
+        message = connectionSocket.recv(BUFFER_SIZE).decode()
         filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = f.read()
-        
-        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-        
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-        
-        connectionSocket.close()
+
+        with open(filename[1:]) as file:
+            outputData = file.read()
+
+        header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+        connectionSocket.send(header.encode())
+        connectionSocket.send(outputData.encode())
 
     except IOError:
-        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n<h1>404 Not Found</h1>".encode())
-        
+        response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+        response += "<h1>404 Not Found</h1><p>The requested file could not be found.</p>"
+        connectionSocket.send(response.encode())
+
+    finally:
         connectionSocket.close()
 
 serverSocket.close()
